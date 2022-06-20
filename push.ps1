3bfb936aace5e12fdf28ee953e2e2dc1ac370937
huhuiyu <1069306849@qq.com>
2022-06-20 16:19:39 +0800

    极狐git提交

--- push.ps1
+++ push.ps1
@@ -1,3 +1,6 @@
+# github链接地址：git remote add github git@github.com:huhuiyu/teach-sources-2021.git
+# jihulab链接地址：git remote add jihulab git@jihulab.com:DarkKnight/teach-sources-2021.git
+# 阿里云链接地址：git@code.aliyun.com:DarkKnight/teach-sources-2021.git
 git push
-git push gitee
+git push jihulab
 git push github

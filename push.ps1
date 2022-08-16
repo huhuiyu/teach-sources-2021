e3afae5daa0ad191aa586510f5327515d1fba555
huhuiyu <1069306849@qq.com>
2022-08-16 16:21:45 +0800

    阿里云地址变更

--- push.ps1
+++ push.ps1
@@ -1,6 +1,6 @@
 # github链接地址：git remote add github git@github.com:huhuiyu/teach-sources-2021.git
 # jihulab链接地址：git remote add jihulab git@jihulab.com:DarkKnight/teach-sources-2021.git
-# 阿里云链接地址：git@code.aliyun.com:DarkKnight/teach-sources-2021.git
+# 阿里云链接地址：git remote add aliyun git@codeup.aliyun.com:62e1fa673c1b5281cd478191/DarkKnight/teach-sources-2021.git
 git push
 git push jihulab
-git push github
+git push aliyun

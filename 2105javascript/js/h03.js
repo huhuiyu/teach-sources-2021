8b3ac86daa0bb5fcf3dc086501db97472f5bec6c
huhuiyu <1069306849@qq.com>
2022-09-21 09:58:05 +0800

    2105js控制样式

---
+++ 2105javascript/js/h03.js
@@ -0,0 +1,35 @@
+console.log('in h03.js======>');
+
+let divStyle = document.getElementById('divStyle');
+console.log(divStyle);
+
+// 通过style属性直通车方法修改元素样式
+// 页面元素.style可以修改元素的style属性值
+// 所有的css样式都可以修改，但是会转换css的-表达式为驼峰
+// 例如background-color为backgroundColor
+divStyle.style.backgroundColor = '#ff0000';
+divStyle.style.color = 'white';
+
+// 修改css属性
+let spDay = document.getElementById('spDay');
+let spNight = document.getElementById('spNight');
+let divCss = document.getElementById('divCss');
+console.log(spDay, spNight, divCss);
+
+// 处理元素的点击功能(事件)
+spDay.addEventListener('click', () => {
+  console.log('点击了spDay');
+  // 元素.classList可以操纵元素的class属性
+  // remove表示移除指定名称的css类
+  // add表示添加指定名称的css类
+  // 注意事项，js并不会验证css类是否存在，所以需要css配合
+  // 多次添加或者移除同一个css属性并不出错，也不会叠加
+  divCss.classList.remove('night');
+  divCss.classList.add('day');
+});
+
+spNight.addEventListener('click', () => {
+  console.log('点击spNight');
+  divCss.classList.remove('day');
+  divCss.classList.add('night');
+});

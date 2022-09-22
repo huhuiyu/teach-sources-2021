e31c5de8b03b322a452fcf55a6500b6e61420389
huhuiyu <1069306849@qq.com>
2022-09-22 18:04:00 +0800

    2103班js控制css

---
+++ 2103javascript/js/h03.js
@@ -0,0 +1,41 @@
+console.log('in h03.js=====>');
+
+let divStyle = document.getElementById('divStyle');
+console.log(divStyle);
+
+// 通过js修改元素的style属性，达到控制样式的目的
+// 页面元素.style可以任意修改元素的样式
+// 所有css属性都会从css减号表达式转换成js的驼峰表达式格式
+// 例如css的border-width的js格式为borderWidth
+divStyle.style.color = '#ff0000';
+divStyle.style.backgroundColor = '#0000ff';
+divStyle.style.fontSize = '2rem';
+divStyle.style.textAlign = 'center';
+
+// 切换主题效果
+let divCss = document.getElementById('divCss');
+let spDay = document.getElementById('spDay');
+let spNight = document.getElementById('spNight');
+console.log(divCss, spDay, spNight);
+
+// js点击事件处理
+// 页面元素.addEventListener('事件类型',()=>{})
+// 表示页面元素触发对应事件时要执行的代码
+spDay.addEventListener('click', () => {
+  console.log('点击了spDay');
+  divCss.classList.remove('night');
+  divCss.classList.add('day');
+});
+
+spNight.addEventListener('click', () => {
+  console.log('点击spNight');
+  // js修改class属性
+  // 元素.classList可以任意修改元素的class属性
+  // add是添加一个class类
+  // remove是移除一个class类
+  // add添加多次相同的class不会叠加
+  // remove移除多次不会报错
+  // 但是js并不会验证class是否存在
+  divCss.classList.remove('day');
+  divCss.classList.add('night');
+});
